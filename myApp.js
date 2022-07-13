527343a8f9fe9971bf930845892d9d76677d8a0e
shingfung <shing2888@gmail.com>
2022-07-13 10:25:00 -0500

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -81,7 +81,10 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, updatedDoc) => {
+    if(err) return console.log(err);
+    done(null, updatedDoc);
+  })
 };
 
 const removeById = (personId, done) => {
